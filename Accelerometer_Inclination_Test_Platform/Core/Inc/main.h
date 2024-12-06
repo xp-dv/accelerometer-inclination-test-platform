58ff0a6131001b8312b3b53f2b258c19cd442b2b
Xavior Pautin <xp.eee116@gmail.com>
2024-12-06 05:16:09 -0800

    .ioc updates to BAUD, servo pins, & TIM4 interrupt

--- Accelerometer_Inclination_Test_Platform/Core/Inc/main.h
+++ Accelerometer_Inclination_Test_Platform/Core/Inc/main.h
@@ -61,13 +61,13 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define BAUD_RATE 115200
+#define BAUD_RATE 38400
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
-#define PWM_X_Pin GPIO_PIN_0
-#define PWM_X_GPIO_Port GPIOA
-#define PWM_Y_Pin GPIO_PIN_1
+#define PWM_Y_Pin GPIO_PIN_0
 #define PWM_Y_GPIO_Port GPIOA
+#define PWM_X_Pin GPIO_PIN_1
+#define PWM_X_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
 #define ADXL_CS_Pin GPIO_PIN_10
--- Accelerometer_Inclination_Test_Platform/Core/Src/main.c
+++ Accelerometer_Inclination_Test_Platform/Core/Src/main.c
@@ -119,6 +119,7 @@ typedef struct setpoint {
 SPI_HandleTypeDef hspi3;
 
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim4;
 
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
@@ -151,6 +152,7 @@ static void MX_USART2_UART_Init(void);
 static void MX_SPI3_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
 
 //* Internal Function Prototypes
@@ -925,6 +927,7 @@ int main(void)
   MX_SPI3_Init();
   MX_USART1_UART_Init();
   MX_TIM2_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
 
   //* Startup State
@@ -1103,6 +1106,51 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 2 */
   HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 100-1;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 20000;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
 
 }
 
